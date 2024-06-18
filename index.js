040d04b98d9bc2e7e6c2c178f25b5b13f38dba3a
ricocanuto <ricardomoraescanuto@gmail.com>
2024-06-18 09:37:29 -0300

    update

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const portDeploy = process.env.PORT || 3000;
+
 const users = []
 
 const checkUserId = (request, response, next) => {
